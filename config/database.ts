b93ec21856932f142d88cb83afc59b16298e0237
Shrikanth <shrikanthbhat30@gmail.com>
2024-11-09 14:03:53 +0530

    setup mongo and mongodb

---
+++ config/database.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+let connected = false;
+
+const connectDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(connected) {
+        console.log("Already connected to mongodb");
+        return;    
+    }
+
+    try {
+      await mongoose.connect(process.env.MONGODB_URI!);
+      connected = true;
+    }
+    catch(error) {
+
+    }
+}
+
+
+export default connectDB

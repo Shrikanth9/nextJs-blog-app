7a1ca45301a399ca797c633315d3c427c98081c4
Shrikanth <shrikanthbhat30@gmail.com>
2024-11-10 14:21:10 +0530

    propertydetails page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperites from "@/components/HomeProperties";
-import connectDB from "@/config/database";
 const Homepage= () => {
     return ( 
         <>
--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,8 +1,35 @@
-const PropertyPage = async( { params }: { params: { id: string }} ) => {
-    const paramsValue = await params;
-    const id = paramsValue.id
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails";
+
+const PropertyPage = async( { params }: { params: any} ) => {
+    await connectDB();
+    const { id } = await params;
+    const property: any = await Property.findById(id).lean();
     return ( 
-      <div> Property Page - {id} </div> 
+      <>
+        <PropertyHeaderImage image={property.images[0]} />
+        <section>
+          <div className="container m-auto py-6 px-6">
+            <Link
+              href="/properties"
+              className="text-blue-500 hover:text-blue-600 flex items-center"
+            >
+              <FaArrowLeft className="mr-2"/> Back to Properties
+            </Link>
+          </div>
+        </section>
+        <PropertyDetails property={property}/>
+         <section className="bg-blue-50">
+          <div className="container m-auto py-10 px-6">
+            <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            </div>
+          </div>
+         </section>
+      </>
     );
 }
  

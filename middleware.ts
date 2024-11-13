b000d48043beae4ce129716c4c561cfdb9ebb4d9
Shrikanth <shrikanthbhat30@gmail.com>
2024-11-13 09:57:09 +0530

    add profile page

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ export { default } from 'next-auth/middleware';
 
 export const config = {
     matcher: [
-        '/:path*',
+        '/properties',
+        '/properties/add',
+        '/profile'
     ]
 }

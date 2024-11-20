29bc190240a1c38db13985745a4bd8cf6dbaef78
Shrikanth9 <76239377+Shrikanth9@users.noreply.github.com>
2024-11-20 06:31:22 +0000

    added pagination

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,11 +1,16 @@
+import Pagination from "@/components/Pagination";
 import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { convertToPlainObj } from "@/utils/Utils";
-const PropertiesPage = async () => {
+const PropertiesPage = async ({searchParams} : any) => {
+   const { page = 1, pageSize = 2 } = await searchParams;
+   const skip = (page - 1) * pageSize;
    await connectDB();
-   let properties = await Property.find({}).lean();
+   const total = await Property.countDocuments({});
+   let properties = await Property.find({}).sort({ createdAt: -1 }).skip(skip).limit(pageSize).lean();
    properties = convertToPlainObj(properties);
+   const showPagination = total > pageSize;
     return ( 
        <section className="px-4 py-6">
           <div className="container-xl lg:container mx-auto px-4 py-6">
@@ -19,6 +24,8 @@ const PropertiesPage = async () => {
                  </div>
              )}
           </div>
+
+          {showPagination && <Pagination page={+page} pageSize={+pageSize} total={+total}/>}
        </section>
     );
 }

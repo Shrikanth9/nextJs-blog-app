c91fc066679c132c5d636378c11dfe9c0dd0c9f5
Shrikanth <shrikantha.ma@juegostudioz.com>
2024-11-29 10:15:04 +0530

    blogdetails image change

--- components/BlogDetails.tsx
+++ components/BlogDetails.tsx
@@ -8,10 +8,10 @@ const BlogDetails = async({ blog }: { blog: IBlog}) => {
         <article className="prose lg:prose-xl mx-3">
             <h1 className="text-2xl md:text-5xl font-bold text-center my-5"> {title} </h1>
             {/* Mobile */}
-            <Image className="md:hidden mx-auto rounded-md shadow-md w-full h-auto" src={image} alt={title} width={800} height={450}/>
+            <Image className="md:hidden mx-auto rounded-md shadow-md w-auto" src={image} alt={title} width={800} height={450}/>
             
             {/* Desktop */}
-            <Image className="hidden md:block mx-auto rounded-md shadow-md max-w-4xl" src={image} alt={title} width={800} height={450}/>
+            <Image className="hidden md:block mx-auto rounded-md shadow-md w-auto" src={image} alt={title} width={800} height={450}/>
 
             <BlogThread blogId={id} totalLikes={totalLikes}/>
             <p className="text-center mt-10 mx-2 whitespace-pre-line"> {content} </p>

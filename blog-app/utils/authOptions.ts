5e1d19f00d7ccc1a9283985bd4d56c1d9c3db137
Shrikanth9 <76239377+Shrikanth9@users.noreply.github.com>
2024-12-01 16:04:09 +0000

    update provder setup

--- blog-app/utils/authOptions.ts
+++ blog-app/utils/authOptions.ts
@@ -2,7 +2,15 @@ import { User } from "@/models/User"
 import Google from "next-auth/providers/google"
 
 export const authOptions = {
-    providers: [Google],
+    providers: [Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),],
     callbacks: {
       authorized: async ({ auth }: any) => {
          // Logged in users are authenticated, otherwise redirect to login page

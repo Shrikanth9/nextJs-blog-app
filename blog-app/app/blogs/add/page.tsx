85343b802aaa486e869fe23a685c61c86c715376
Shrikanth9 <76239377+Shrikanth9@users.noreply.github.com>
2024-12-02 11:11:09 +0000

    style changes for addblog page

--- blog-app/app/blogs/add/page.tsx
+++ blog-app/app/blogs/add/page.tsx
@@ -4,7 +4,7 @@ const AddBlogPage = () => {
     return ( 
         <section className="mt-5">
             <div className="container mx-auto max-w-7xl">
-                <div className="bg-white px-6 py-8 m-4 shadow-md rounded-md border md:m-0">
+                <div className="bg-white px-6 py-6 m-8 shadow-md rounded-md border">
                     <AddBlogForm />
                 </div>
             </div>
